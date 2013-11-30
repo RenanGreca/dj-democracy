56d57c0d72fc75304282afc45e491edd158172cf
Renan Greca <renangreca@gmail.com>
2013-11-30 14:41:08 -0800

    tutorial project

---
+++ ToDoList/ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Renan Greca on 11/30/13.
+//  Copyright (c) 2013 Renan Greca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
