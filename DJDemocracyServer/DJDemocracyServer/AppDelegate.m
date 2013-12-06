9852350c0d51217005039405574ab80e65753344
Mauricio Molina <mmolina@cs227-11.cs.pomona.edu>
2013-12-06 14:52:42 -0800

    Add DJDemocracyServer project and include EyeTunes framework

---
+++ DJDemocracyServer/DJDemocracyServer/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  DJDemocracyServer
+//
+//  Created by Mauricio Molina on 12/6/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ DJDemocracyServer/DJDemocracyServer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DJDemocracyServer
+//
+//  Created by Mauricio Molina on 12/6/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
