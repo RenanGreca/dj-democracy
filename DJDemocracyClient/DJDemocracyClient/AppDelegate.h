78f3170d62c09fbb46ec395d5fc6030d8fefe472
Andy Russell <arussell123@gmail.com>
2013-12-12 14:46:03 -0800

    add initial server connection

--- DJDemocracyClient/DJDemocracyClient/AppDelegate.h
+++ DJDemocracyClient/DJDemocracyClient/AppDelegate.h
@@ -7,9 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Server.h"
+#import "ViewController.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) Server *_server;
+@property (strong, nonatomic) NSArray *servers;
+@property (strong, nonatomic) NSArray *playlist;
+@property (strong, nonatomic) ViewController *serverViewController;
 
 @end
