543e79add898b5927151b1d24d4210a0d74b416d
Renan Greca <renangreca@gmail.com>
2013-12-13 11:46:13 -0800

    Server

--- DJDemocracyServer/DJDemocracyServer/AppDelegate.h
+++ DJDemocracyServer/DJDemocracyServer/AppDelegate.h
@@ -8,19 +8,29 @@
 
 #import <Cocoa/Cocoa.h>
 #import "Server.h"
+#import <EyeTunes/EyeTunes.h>
+
 
 @interface AppDelegate : NSObject <NSApplicationDelegate, ServerDelegate> {
-    IBOutlet NSTableView *tableView;
+    IBOutlet NSTableView *playlistTable;
+    IBOutlet NSTableView *deviceTable;
     NSWindow *window;
     Server *_server;
     NSMutableArray *_playlists;
     NSMutableArray *_services;
+    NSString *textToSend, *_message;
+	NSInteger selectedRow, connectedRow;
+	BOOL isConnectedToService;
 }
 
 @property (assign) IBOutlet NSWindow *window;
 @property (nonatomic, retain) Server *server;
 @property (nonatomic, retain) NSMutableArray *playlists;
 @property (nonatomic, retain) NSMutableArray *services;
+@property(readwrite, copy) NSString *message;
+@property(readwrite, nonatomic) BOOL isConnectedToService;
+
+
 
 - (IBAction)connectToService:(id)sender;
 - (IBAction)sendSongs:(id)sender;
--- DJDemocracyServer/DJDemocracyServer/AppDelegate.m
+++ DJDemocracyServer/DJDemocracyServer/AppDelegate.m
@@ -29,6 +29,116 @@
     if(![_server start:&error]) {
         NSLog(@"error = %@", error);
     }
+    
+    [self showPlaylists];
+    
+    
+}
+
+- (void)showPlaylists; {
+    EyeTunes *eyetunes = [EyeTunes sharedInstance];
+    NSArray *playlists = [eyetunes playlists];
+    if (!playlists)
+        return;
+    
+    NSLog(@"List of playlists:");
+    for (id playlist in playlists) {
+        [self.playlists addObject:playlist];
+        NSLog(@"Playlist: %@",[playlist name]);
+    }
+    
+    [playlistTable reloadData];
+    
+    /*NSLog(@"Track: %@",[currentTrack name]);
+     NSLog(@"Artist: %@",[currentTrack albumArtist]);*/
+
+}
+
+
+- (IBAction)connectToService:(id)sender;
+{
+	[self.server connectToRemoteService:[self.services objectAtIndex:selectedRow]];
+}
+
+- (IBAction)sendText:(id)sender;
+{
+	NSData *data = [textToSend dataUsingEncoding:NSUTF8StringEncoding];
+	NSError *error = nil;
+	[self.server sendData:data error:&error];
+	
+    //[self.server s ]
+}
+
+- (IBAction)sendSongs:(id)sender; {
+    
+}
+
+#pragma mark -
+#pragma mark Server delegate methods
+
+- (void)serverRemoteConnectionComplete:(Server *)server
+{
+    NSLog(@"Connected to service");
+	
+	self.isConnectedToService = YES;
+    
+	connectedRow = selectedRow;
+	[deviceTable reloadData];
+}
+
+- (void)serverStopped:(Server *)server
+{
+    NSLog(@"Disconnected from service");
+    
+	self.isConnectedToService = NO;
+    
+	connectedRow = -1;
+	[deviceTable reloadData];
+}
+
+- (void)server:(Server *)server didNotStart:(NSDictionary *)errorDict
+{
+    NSLog(@"Server did not start %@", errorDict);
+}
+
+- (void)server:(Server *)server didAcceptData:(NSData *)data
+{
+    NSLog(@"Server did accept data %@", data);
+    //NSString *message = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
+    /*if(nil != message || [message length] > 0) {
+        self.message = message;
+    } else {
+        self.message = @"no data received";
+    }*/
+}
+
+- (void)server:(Server *)server lostConnection:(NSDictionary *)errorDict
+{
+	NSLog(@"Lost connection");
+	
+	self.isConnectedToService = NO;
+	connectedRow = -1;
+	[deviceTable reloadData];
+}
+
+- (void)serviceAdded:(NSNetService *)service moreComing:(BOOL)more
+{
+	NSLog(@"Added a service: %@", [service name]);
+	
+    [self.services addObject:service];
+    if(!more) {
+        [deviceTable reloadData];
+    }
+}
+
+- (void)serviceRemoved:(NSNetService *)service moreComing:(BOOL)more
+{
+	NSLog(@"Removed a service: %@", [service name]);
+	
+    [self.services removeObject:service];
+    if(!more) {
+        [deviceTable reloadData];
+    }
 }
 
 @end
