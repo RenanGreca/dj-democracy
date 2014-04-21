e241c38531c42c1c861075f08302301d182282dd
Mari Bennett <mbennett@fir.cs.hmc.edu>
2014-04-20 18:51:48 -0700

    whoops. Reenabling vote-flag.

--- DJDemocracyServer/DJDemocracyServer/AppDelegate.m
+++ DJDemocracyServer/DJDemocracyServer/AppDelegate.m
@@ -247,7 +247,7 @@ NSInteger voteSort(id num1, id num2, void *context) {
         [self.server sendData:data error:&error];
     }
     [NSThread sleepForTimeInterval:0.01f];
-    //data = [@"Done" dataUsingEncoding:NSUTF8StringEncoding];
+    data = [@"Done" dataUsingEncoding:NSUTF8StringEncoding];
     [self.server sendData:data error:&error];
 }
 
